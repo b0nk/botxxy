7c3e0709b8f9defdc98e927474727186ee38d8f8
b0nk <fsantos.pt@hotmail.com>
2014-03-14 00:40:55 +0000

    fix empty titles and unescaped html entities

--- src/hb.py
+++ src/hb.py
@@ -1,10 +1,10 @@
 import requests
 import bs4
 
-from mylib import stripHTML
+from mylib import stripHTML, unescape
 
 def getGames():
   soup  = bs4.BeautifulSoup(requests.get("http://humblebundle.com").text)
-  games = [str.strip(stripHTML(str(i))) for i in soup.find_all('span', 'item-title')]
+  games = [ unescape(str.strip(stripHTML(str(i)))) for i in soup.find_all('span', 'item-title') if str.strip(stripHTML(str(i))) is not '' ]
   
   return ", ".join(games)
