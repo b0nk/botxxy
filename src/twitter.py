e60a8810a182d6abb8d5153aabbe1e7779fd4c4b
b0nk <fsantos.pt@hotmail.com>
2014-03-19 15:39:22 +0000

    removed bs4 / httplib2 / requests

--- src/twitter.py
+++ src/twitter.py
@@ -3,15 +3,16 @@ import time
 import datetime
 from mylib import unescape, myprint
 
-  
-def getTweet(user, n):
-  now = int(time.time())
-  t_logo = "0,10twitter"
-  try:
-    t_api = pytwitter.Api(consumer_key = '***REMOVED***',
+t_logo = "0,10twitter"
+
+t_api = pytwitter.Api(consumer_key = '***REMOVED***',
                           consumer_secret = '***REMOVED***',
                           access_token_key = '***REMOVED***',
                           access_token_secret = '***REMOVED***')
+
+def getTweet(user, n):
+  now = int(time.time())
+  try:
     
     t_user = t_api.GetUser(None, user)._screen_name
     tweets = t_api.GetUserTimeline(screen_name = t_user, count = 200)
@@ -22,7 +23,7 @@ def getTweet(user, n):
     else:
       tweet = tweets[n].GetText()
       t = int(tweets[n].GetCreatedAtInSeconds())
-      created = "Posted %s ago" % (datetime.timedelta(seconds=now-t))
+      created = "Posted %s ago" % (datetime.timedelta(seconds = now - t))
       tweet = unescape(tweet).replace('\n', ' ')
       return "%s @%s: %s (%s)" % (t_logo, t_user, tweet, created)
       
@@ -31,3 +32,15 @@ def getTweet(user, n):
     return "%s Error: Nope." % (t_logo)
   except IndexError:
     return "%s Error: You have gone too far (keep below 200)" % (t_logo)
+
+def search(query, n):
+  try:
+    results = t_api.GetSearch(term = query, result_type = "recent", count = 15)
+    if not results:
+      return "%s No results for %s" % (t_logo, query)
+    else:
+      user = results[n].GetUser()._screen_name
+      tweet = unescape(results[n].GetText()).replace('\n', ' ')
+      return "%s @%s: %s" % (t_logo, user, tweet)
+  except IndexError:
+    return "%s Error: YOU'VE GONE TOO FAR (keep below 15)" % (t_logo)
